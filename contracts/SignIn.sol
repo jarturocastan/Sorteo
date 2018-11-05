f3db553bb0c9bdf5ce05c1bc6e3990720fe8dbce
DESKTOP-I123GOJ\José Arturo <Jose Arturo Castan>
2018-11-05 14:14:11 -0500

    Función de registro

--- contracts/SignIn.sol
+++ contracts/SignIn.sol
@@ -1,15 +1,55 @@
 pragma solidity 0.4.24;
 
-contract SignIn { 
+contract SignIn {
+    address owner;
+    address manager; 
+    
+    event RegisterEvent(
+        address userAccount,
+        uint256 block_number,
+        bytes32 block_hash
+    );
+
+    event UpdateTicketAmountEvent(
+        address userAccount,
+        uint256 block_number,
+        bytes32 block_hash
+    );
+
     struct Register  {
         address account;
         int256 tickets_amount;
         uint created_at;
+        uint updated_at;
     }
 
-    mapping(bytes32 => Register) registers; 
+    mapping(address => Register) registers; 
+     
+    function register(address userAccount,int256 ticket_amount) public {
+        registers[userAccount].account = userAccount;
+        registers[userAccount].tickets_amount = ticket_amount;
+        registers[userAccount].created_at = block.timestamp;   
+        registers[userAccount].updated_at = block.timestamp; 
+        emit RegisterEvent(registers[userAccount].account, block.number, blockhash(block.number));
+    }
 
+    function updateTicketAmount(address userAccount,int256 ticket_amount) public {
+        registers[userAccount].tickets_amount = ticket_amount;  
+        registers[userAccount].updated_at = block.timestamp;
+        emit UpdateTicketAmountEvent(registers[userAccount].account, block.number, blockhash(block.number)); 
+    }
+
+    modifier onlyOwner() {
+        require(owner == msg.sender); _;
+    }
     
+    modifier onlyManager() {
+        require(manager == msg.sender); _;
+    }
 
+    modifier onlyManagerOrOwner() {
+        require(owner == msg.sender || manager == msg.sender); _;
+    }
+    
 }
 
