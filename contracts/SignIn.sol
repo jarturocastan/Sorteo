15a95a4888f264f1e00329db1ffc4b79be90b4e4
dpat <davidpat.sistemas@gmail.com>
2018-11-13 15:09:20 -0500

    holi bebe

--- contracts/SignIn.sol
+++ contracts/SignIn.sol
@@ -16,41 +16,56 @@ contract SignIn {
         bytes32 block_hash
     );
 
+    struct Game {
+        uint id_game;
+        string  game_name;
+    }
+
+    struct Sorteo {
+        uint id_sorteo;
+        string sorteo_name;
+    }
+
     struct Register  {
         address account;
         address walletFundation;
         int256[] numbers;
         string amount_to_foundation;
+        uint ticketNumber;
         uint created_at;
         uint updated_at;
         string total;
-        uint id_sorteo;
+        Sorteo sorteo;
+        Game game;
     }
+
+
+
 
     mapping(address => Register) registers; 
     
     constructor() public {
 		owner = msg.sender;
+        manager = msg.sender;
 	}
-
-     
-    function register(address walletUser,
-    int256[] numbers, address walletFundation, 
-    string amount_to_foundation, string total) public payable onlyManagerOrOwner returns(address,address, int256[], string, uint,uint,string,uint) {
-        registers[walletUser].account = walletUser;
-        registers[walletUser].numbers = numbers;
-        registers[walletUser].walletFundation = walletFundation; 
-        registers[walletUser].created_at = block.timestamp;   
-        registers[walletUser].updated_at = block.timestamp; 
-        registers[walletUser].total = total; 
-        registers[walletUser].id_sorteo = 1; 
-        registers[walletUser].amount_to_foundation = amount_to_foundation; 
-        walletFundation.transfer(msg.value);
-        emit RegisterEvent(registers[walletUser].account, block.number, blockhash(block.number));
-        return (walletUser,walletFundation,numbers,amount_to_foundation,block.timestamp,block.timestamp,total,1);
+    
+    function register(address walletUser, address walletFundation,int256[] numbers, 
+        string total,uint ticketNumber,string amount_to_foundation, uint id_sorteo, string sorteo_name,uint id_game ,string game_name) 
+        public payable onlyManagerOrOwner  {
+            registers[walletUser].account = walletUser;
+            registers[walletUser].numbers = numbers;
+            registers[walletUser].walletFundation = walletFundation; 
+            registers[walletUser].created_at = block.timestamp;   
+            registers[walletUser].updated_at = block.timestamp; 
+            registers[walletUser].total = total; 
+            registers[walletUser].amount_to_foundation = amount_to_foundation; 
+            registers[walletUser].ticketNumber = ticketNumber;
+            registers[walletUser].sorteo = Sorteo({id_sorteo : id_sorteo,  sorteo_name : sorteo_name});
+            registers[walletUser].game = Game({id_game : id_game,game_name : game_name});
+            walletFundation.transfer(msg.value);
+            emit RegisterEvent(registers[walletUser].account, block.number, blockhash(block.number));
     }
 
-    
     function setManager(address _manager) public onlyOwner {
         manager = _manager;
     }
--- contracts/Sorteo.sol
+++ contracts/Sorteo.sol
@@ -4,12 +4,44 @@ import "./SignIn.sol";
 
 contract Sorteo {
     address owner;
-    address manager; 
+    address manager;
 
+    constructor(address address_contract) public {
+		owner = msg.sender;
+        manager = msg.sender;
+        SignIn signin(address_contract);
+	}
 
-    function determinateWinners() public {
+    struct WinnerSerieSelected {
+        int256[] WinnerSerie;
+        uint created_at;
+        uint updated_at;
+        uint amount_to_transfer;
+    } 
 
+    
+    function determinateWinners(uint id_game , string game_name, uint id_sorteo, string sorteo_name, int256[] WinnerSerie, uint ticketNumber, uint amount_to_transfer) public {
+        
+        
     }
+
+    function TransferToWinnner(address walletUser) private payable {
+        amount_to_transfer = msg.value;
+        walletUser.transfer(msg.value);
+        
+    }
+
+    function kill() public onlyOwner {
+        selfdestruct(owner);
+    }
+
+    modifier onlyOwner() {
+        require(owner == msg.sender); _;
+    }
+
+    modifier onlyManagerOrOwner() {
+        require(owner == msg.sender || manager == msg.sender); _;
+    }   
     
 
 
