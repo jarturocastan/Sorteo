b44ebadb3dd57853a7f2113ddae510a7ed41a1de
DESKTOP-I123GOJ\José Arturo <Jose Arturo Castan>
2018-11-13 10:48:31 -0500

    modificaciones del smart contract Signin

--- contracts/Sorteo.sol
+++ contracts/Sorteo.sol
@@ -5,14 +5,31 @@ import "./SignIn.sol";
 contract Sorteo {
     address owner;
     address manager; 
+    SignIn signIn;
 
+    constructor(address address_contract) public {
+		owner = msg.sender;
+        manager = msg.sender;
+         signIn = SignIn(address_contract);
+	}
 
-    function determinateWinners() public {
+    function determinateWinners(uint id_sorteo, uint id_game, uint ticketNumber, uint amount, int256[] numbers) public {
 
     }
+
+    function setSignIn(address address_contract) {
+        signIn = SignIn(address_contract);
+    }
     
+    function kill() public onlyOwner {
+        selfdestruct(owner);
+    }
 
+    modifier onlyOwner() {
+        require(owner == msg.sender); _;
+    }
 
-
-
+    modifier onlyManagerOrOwner() {
+        require(owner == msg.sender || manager == msg.sender); _;
+    }   
 }
