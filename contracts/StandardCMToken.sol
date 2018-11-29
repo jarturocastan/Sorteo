352aaae1e1144d56432ca87f6264005245ab6218
dpat <davidpat.sistemas@gmail.com>
2018-11-29 15:48:27 -0500

    fixed

--- contracts/StandardCMToken.sol
+++ contracts/StandardCMToken.sol
@@ -1,26 +1,6 @@
 pragma solidity ^0.4.24;
 import "./ERC20.sol";
 
-
-library SafeMath {
-    function add(uint a, uint b) internal pure returns (uint c) {
-        c = a + b;
-        require(c >= a);
-    }
-    function sub(uint a, uint b) internal pure returns (uint c) {
-        require(b <= a);
-        c = a - b;
-    }
-    function mul(uint a, uint b) internal pure returns (uint c) {
-        c = a * b;
-        require(a == 0 || c / a == b);
-    }
-    function div(uint a, uint b) internal pure returns (uint c) {
-        require(b > 0);
-        c = a / b;
-    }
-}
-
 contract TimeTest {
     
     function currentTime() public view returns (uint) {
@@ -30,7 +10,6 @@ contract TimeTest {
 
 contract StandardCMToken is ERCCM20 {
     
-    using SafeMath for uint256;
     uint256 public priceInSzabo = 1000;
     uint public sdate1 = 1543190400; // 26 noviembre 2018
     uint public edate1 = 1544832000; //15 de diciembre 2018
@@ -51,20 +30,116 @@ contract StandardCMToken is ERCCM20 {
     uint256 public totalSupply;
     
 // NEW ONES    
+    
 
-     
-        function transfer(address _to, uint256 _value) public  returns (bool success) {
-        if (balances[msg.sender] >= _value && _value > 0) {
-            balances[msg.sender] -= _value;
-            balances[_to] += _value;
-            emit TransferEvent(msg.sender, _to, _value);
-            return true;
-        } else { 
-            return false; 
-        }
+
+    /* This generates a public event on the blockchain that will notify clients */
+    
+//
+    
+    
+    
+    function transfer(address _to, uint256 _value) public payable  {
+        
+        uint256 checker = now;
+        //Default assumes totalSupply can't be over max (2^256 - 1).
+        //If your token leaves out totalSupply and can issue more tokens as time goes on, you need to check if it doesn't wrap.
+        // Replace the if with this one instead.
+        //if (balances[msg.sender] >= _value && balances[_to] + _value > balances[_to]) {
+        //if (balances[address(this)] >= _value && _value > 0) {
+        //    balances[address(this)] -= _value;
+        //    balances[_to] += _value;
+        //    emit TransferEvent(address(this), _to, _value);
+        //    return true;
+        //} else { 
+        //    return false; 
+        //}
+     if ((checker > sdate1) && (checker < edate1)) {
+         if (balances[address(this)] >= _value && _value > 0) {
+             total = _value * priceInSzabo;
+             extra = _value / 2;
+             totalValue = (msg.value / 1000000000000000000) * 1000000;
+             if (total == totalValue) {
+                 balances[msg.sender] = balances[msg.sender] + (_value + extra);
+                 balances[address(this)] = balances[address(this)] - (_value + extra);
+                 owner.transfer(msg.value);
+                 emit TransferEvent(address(this), _to, _value);
+             } else {
+                 revert();
+             }
+         } else {
+             revert();
+         }
+     } else if ((checker > sdate2) && (checker < edate2)) {
+         if (balances[address(this)] >= _value && _value > 0) {
+             total = _value * 1600;
+             extra = _value * 2 / 5;
+             totalValue = (msg.value / 1000000000000000000) * 1000000;
+             if (total == totalValue) {
+                 balances[msg.sender] = balances[msg.sender] + (_value + extra);
+                 balances[address(this)] = balances[address(this)] - (_value + extra);
+                 owner.transfer(msg.value);
+                 emit TransferEvent(address(this), _to, _value);
+             } else {
+                 revert();
+             }
+         } else {
+             revert();
+         }
+     } else if ((checker > sdate3) && (checker < edate3)) {
+         if (balances[address(this)] >= _value && _value > 0) {
+             total = _value * 2400;
+             totalValue = (msg.value / 1000000000000000000) * 1000000;
+             extra = _value * 3 / 10;
+             if (total == totalValue) {
+                 balances[msg.sender] = balances[msg.sender] + (_value + extra);
+                 balances[address(this)] = balances[address(this)] - (_value + extra);
+                 owner.transfer(msg.value);
+                 emit TransferEvent(address(this), _to, _value);
+             } else {
+                 revert();
+             }
+         } else {
+             revert();
+         }
+     } else if ((checker > sdate4) && (checker < edate4)) {
+         if (balances[address(this)] >= _value && _value > 0) {
+             total = _value * 3500;
+             totalValue = (msg.value / 1000000000000000000) * 1000000;
+             extra = _value * 1 / 5;
+             if (total == totalValue) {
+                 balances[msg.sender] = balances[msg.sender] + (_value + extra);
+                 balances[address(this)] = balances[address(this)] - (_value + extra);
+                 owner.transfer(msg.value);
+                 emit TransferEvent(address(this), _to, _value);
+             } else {
+                 revert();
+             }
+         } else {
+             revert();
+         }
+     } else if ((checker > sdate5) && (checker < edate5)) {
+         if (balances[address(this)] >= _value && _value > 0) {
+             total = _value * 5300; //265
+             totalValue = (msg.value / 1000000000000000000) * 1000000;
+             extra = total * 1 / 20;
+             if (total == totalValue) {
+                 balances[msg.sender] = balances[msg.sender] + (_value + extra);
+                 balances[address(this)] = balances[address(this)] - (_value + extra);
+                 owner.transfer(msg.value);
+                 emit TransferEvent(address(this), _to, _value);
+             } else {
+                 revert();
+             }
+         } else {
+             revert();
+         }
+     }
     }
 
     function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        //same as above. Replace this line with the following if you want to protect against wrapping uints.
+        //if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && balances[_to] + _value > balances[_to]) {
         if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && _value > 0) {
             balances[_to] += _value;
             balances[_from] -= _value;
@@ -76,50 +151,137 @@ contract StandardCMToken is ERCCM20 {
         }
     }
 
-    function balanceOf(address _owner)  public constant returns (uint256 balance) {
+    function balanceOf(address _owner) public constant returns (uint256 balance) {
         return balances[_owner];
     }
 
-    function approve(address _spender, uint256 _value) public  returns (bool success) {
+    function approve(address _spender, uint256 _value) public returns (bool success) {
         allowed[msg.sender][_spender] = _value;
         emit ApprovalEvent(msg.sender, _spender, _value);
         return true;
     }
 
     function allowance(address _owner, address _spender) public constant returns (uint256 remaining) {
-        return allowed[_owner][_spender];
+      return allowed[_owner][_spender];
+    }
+    
+ 
+    /*
+     * Internal transfer, only can be called by this contract
+     */
+    function transfernew(address _from, address _to, uint _value) internal {
+        // Prevent transfer to 0x0 address. Use burn() instead
+        require(_to != 0x0);
+        // Check if the sender has enough
+        require(balances[_from] >= _value);
+        // Check for overflows
+        require(balances[_to] + _value > balances[_to]);
+        // Save this for an assertion in the future
+        uint previousBalances = balances[_from] + balances[_to];
+        // Subtract from the sender
+        balances[_from] -= _value;
+        // Add the same to the recipient
+        balances[_to] += _value;
+        emit TransferEvent(_from, _to, _value);
+        // Asserts are used to use static analysis to find bugs in your code. They should never fail
+        assert(balances[_from] + balances[_to] == previousBalances);
     }
 
-    function sellToken(uint256 _amount) public payable {
-        TimeTest time = new TimeTest();
-        uint checker = time.currentTime();
-         if ((checker > sdate1) && (checker < edate1)) {
-            if(balances[address(this)] >= _amount && _amount > 0) {
-                total = _amount * priceInSzabo;
-                extra = _amount / 2;
-                totalValue = msg.value * 1000000;
-                if(total == totalValue) {
-                    balances[msg.sender] = balances[msg.sender] + (_amount + extra);
-                    balances[address(this)] =  balances[address(this)] - (_amount + extra);
-                    owner.transfer(msg.value);
-                    //transfer(address(this),add_to);
-                    //transferFrom(address(this),owner,msg.value);
-                    //transferFrom(address(this),msg.sender,add_to);
-                    //address(this).transfer(msg.value);
-                } else {
-                    revert();
-            }
-        } else {
-            revert();
-        }
+ 
 
-            
-        }else{
-         revert();
-     }
-        
-        
-        
-     }
-   
+
+    /*
+     * Destroy tokens
+     *
+     * Remove `_value` tokens from the system irreversibly
+     *
+     * @param _value the amount of money to burn
+     */
+
+
+    
+    function Time_call() public view returns (uint256){
+        return now;
+    }
+    
+  //  function buyToken(uint256 _amount) public payable {
+//     uint256 checker = now;
+  //   if ((checker > sdate1) && (checker < edate1)) {
+    //     if (balances[address(this)] >= _amount && _amount > 0) {
+     //        total = _amount * priceInSzabo;
+     //        extra = _amount / 2;
+     //        totalValue = (msg.value / 1000000000000000000) * 1000000;
+     //        if (total == totalValue) {
+     //            balances[msg.sender] = balances[msg.sender] + (_amount + extra);
+     //            balances[address(this)] = balances[address(this)] - (_amount + extra);
+     //            owner.transfer(msg.value);
+     //        } else {
+     //            revert();
+     //        }
+     //    } else {
+     //        revert();
+     //    }
+    // } else if ((checker > sdate2) && (checker < edate2)) {
+     //    if (balances[address(this)] >= _amount && _amount > 0) {
+      //       total = _amount * 1600;
+    //         extra = _amount * 2 / 5;
+     //        totalValue = (msg.value / 1000000000000000000) * 1000000;
+     //        if (total == totalValue) {
+      //           balances[msg.sender] = balances[msg.sender] + (_amount + extra);
+    //             balances[address(this)] = balances[address(this)] - (_amount + extra);
+    //             owner.transfer(msg.value);
+     //        } else {
+     //            revert();
+      //       }
+        // } else {
+    //         revert();
+     //    }
+     //} else if ((checker > sdate3) && (checker < edate3)) {
+     //    if (balances[address(this)] >= _amount && _amount > 0) {
+     //        total = _amount * 2400;
+     //        totalValue = (msg.value / 1000000000000000000) * 1000000;
+     //        extra = _amount * 3 / 10;
+     //        if (total == totalValue) {
+     //            balances[msg.sender] = balances[msg.sender] + (_amount + extra);
+     //            balances[address(this)] = balances[address(this)] - (_amount + extra);
+     //            owner.transfer(msg.value);
+     //        } else {
+     //            revert();
+     //        }
+     //    } else {
+     //        revert();
+     //    }
+    // } else if ((checker > sdate4) && (checker < edate4)) {
+    //     if (balances[address(this)] >= _amount && _amount > 0) {
+    //         total = _amount * 3500;
+    //        totalValue = (msg.value / 1000000000000000000) * 1000000;
+    //         extra = _amount * 1 / 5;
+    //         if (total == totalValue) {
+    //             balances[msg.sender] = balances[msg.sender] + (_amount + extra);
+     //            balances[address(this)] = balances[address(this)] - (_amount + extra);
+     //            owner.transfer(msg.value);
+     //        } else {
+     //            revert();
+     //        }
+      //   } else {
+    //         revert();
+    //     }
+    // } else if ((checker > sdate5) && (checker < edate5)) {
+    //     if (balances[address(this)] >= _amount && _amount > 0) {
+    //         total = _amount * 5300; //265
+    //         totalValue = (msg.value / 1000000000000000000) * 1000000;
+    //         extra = total * 1 / 20;
+    //         if (total == totalValue) {
+    //             balances[msg.sender] = balances[msg.sender] + (_amount + extra);
+    //             balances[address(this)] = balances[address(this)] - (_amount + extra);
+     //            owner.transfer(msg.value);
+     //        } else {
+     //            revert();
+     //        }
+      //   } else {
+    //         revert();
+     //    }
+    // }
+ //}
+       
 }
